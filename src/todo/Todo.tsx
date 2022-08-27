1f69916f42e1e19e5fc3439fead4668a35c11f8b
Víctor Tamayo <victortamayo509@gmail.com>
2022-08-27 12:46:43 -0500

    ts-todo-app

---
+++ src/todo/Todo.tsx
@@ -0,0 +1,13 @@
+function Todo() {
+  return (
+    <>
+        <h1>Todo: </h1>
+        <ul>
+            <li>TODO 1</li>
+            <li>TODO 2</li>
+            <li>TODO 3</li>
+        </ul>
+    </>
+  )
+}
+export default Todo
